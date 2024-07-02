1bfa8f446d018105dc81ff267d19a4542079d4d9
Queenson9 <eqeduafo@st.ug.edu.gh>
2024-07-01 18:26:41 -0700

    setup project

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import Cart from './screens/Cart';
 
 export default function App() {
+const Stack = createStackNavigator()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen options={{headerShown: false}} name='Home' component={Home}/>
+      <Stack.Screen options={{headerShown: false}} name='Cart' component={Cart}/>
+    </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
